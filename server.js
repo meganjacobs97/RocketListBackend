fd3e4e99a5e4840638edad556e0c26352239c92b
Megan Jacobs <meganjacobs@live.com>
2020-05-19 13:32:56 -0700

    cors

--- server.js
+++ server.js
@@ -20,12 +20,10 @@ if (process.env.NODE_ENV === "production") {
 }
 
 //allow cross-server requests - TODO: specify deployed sites 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-app.use( cors()); 
+
+app.use( cors({
+  origin: ["localhost:3000"]
+})); 
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/rocketlist");
